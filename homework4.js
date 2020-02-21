06f32f555669157a0a6e31885b0bcaf5baccdd5f
Shino99999 <donicamiku1@icloud.com>
2020-02-22 00:31:10 +0900

    Initial commit

---
+++ homework4.js
@@ -0,0 +1,6 @@
+var send_string = document.getElementById("caption_input");
+var new_string = document.getElementById("string");
+
+function changeString() {
+    new_string.innerText = send_string.value;
+}
