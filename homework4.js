c738c1927d9af7e326f8df1f7e18baf57eadf27b
Shino99999 <donicamiku1@icloud.com>
2020-03-02 16:57:33 +0900

    changed homework4.js

--- homework4.js
+++ homework4.js
@@ -1,6 +1,6 @@
-var send_string = document.getElementById("caption_input");
-var new_string = document.getElementById("string");
-
 function changeString() {
+    var send_string = document.getElementById("#caption_input");
+    var new_string = document.getElementById("#string");
+
     new_string.innerText = send_string.value;
 }
